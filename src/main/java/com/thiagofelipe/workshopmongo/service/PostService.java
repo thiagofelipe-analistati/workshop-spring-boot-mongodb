86c64bc9a4720689c761a915a56b37106fce52ac
Thiago Felipe <thiagofelipe_analistati@outlook.com>
2021-05-11 16:33:35 -0300

    Buscar Post por ID

---
+++ src/main/java/com/thiagofelipe/workshopmongo/service/PostService.java
@@ -0,0 +1,27 @@
+package com.thiagofelipe.workshopmongo.service;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.thiagofelipe.workshopmongo.dominio.Post;
+import com.thiagofelipe.workshopmongo.repository.PostRepository;
+import com.thiagofelipe.workshopmongo.service.excecoes.ObjetoNaoEncontrado;
+
+@Service
+public class PostService {
+	
+	@Autowired //
+	private PostRepository repository;
+	
+	
+
+	public Post findById(String id){
+		Optional<Post> obj = repository.findById(id);
+		
+		return obj.orElseThrow(()-> new ObjetoNaoEncontrado("Obejto não encontrado"));
+		
+	}
+	
+}
